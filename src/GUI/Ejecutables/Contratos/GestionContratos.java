af2b6d1a0f5435579b36f7acb854f18c7d5e2c56
305660224 <denismarchena9@gmail.com>
2025-08-11 11:42:03 -0600

    Se crearon 6 JPanel y 1 JFrame
    Se crearon 6 JPanel correspondientes alos apartados de gestion respectivos a vehiculos, clientes, empleados, contratos, menu_principal, y reservas, ademas de la creacion del JFrame, el cual, dentro de su codigo cuenta con la inicializacion y el .setsize de cada JPanel mencionado, ademas del add(menuprincipal_jpanel) que sera el JPnale que se ejecutara primero cuando se ejecute el JFrame, tanto el JFrame como los JPanel trabajan con tamaños de 1920x1800px

---
+++ src/GUI/Ejecutables/Contratos/GestionContratos.java
@@ -0,0 +1,37 @@
+
+package GUI.Ejecutables.Contratos;
+
+import GUI.Ejecutables.*;
+
+/**
+ *
+ * @author denis
+ */
+public class GestionContratos extends javax.swing.JPanel {
+
+    public GestionContratos() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setPreferredSize(new java.awt.Dimension(1920, 1800));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/GUI/Ejecutables/MainFrame.java
@@ -0,0 +1,86 @@
+
+package GUI.Ejecutables;
+import GUI.Ejecutables.Clientes.GestionClientes;
+import GUI.Ejecutables.Empleados.GestionEmpleados;
+import GUI.Ejecutables.Vehiculos.GestionVehiculos;
+import GUI.Ejecutables.Contratos.GestionContratos;
+import GUI.Ejecutables.Reservas.GestionReservas;
+
+/**
+ * 11/08/2025
+ * @author denis
+ */
+public class MainFrame extends javax.swing.JFrame {
+    
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(MainFrame.class.getName());
+    
+    //Inicializacion de Jpanels
+    MenuPrincipal menuprincipal_jpanel = new MenuPrincipal();
+    GestionEmpleados gestionempleado_jpanel = new GestionEmpleados();
+    GestionClientes gestioncliente_jpanel = new GestionClientes();
+    GestionVehiculos gestionvehiculo_jpanel = new GestionVehiculos();
+    GestionContratos gestioncontrato_jpanel = new GestionContratos();
+    GestionReservas gestionreserva_jpanel = new GestionReservas();
+    
+    
+    /**
+     * Creates new form MainFrame
+     */
+    public MainFrame() {
+        initComponents();        
+    //Inicializar tamaño de Jpanels
+    menuprincipal_jpanel.setSize(1920, 1800);
+    gestionempleado_jpanel.setSize(1920, 1800);
+    gestioncliente_jpanel.setSize(1920, 1800);
+    gestionvehiculo_jpanel.setSize(1920, 1800);
+    gestioncontrato_jpanel.setSize(1920, 1800);
+    gestionreserva_jpanel.setSize(1920, 1800);
+    
+    //Inicio por default
+        add(menuprincipal_jpanel);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1920, 1800));
+        setSize(new java.awt.Dimension(1920, 1800));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1520, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1500, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new MainFrame().setVisible(true));
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
