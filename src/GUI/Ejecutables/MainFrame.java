c0de0e5ef742c0faa22466456f1327f3e5b880e4
305660224 <denismarchena9@gmail.com>
2025-08-27 19:33:14 -0600

    Se modifico el JFrame
    Se le añadieron instancias de cada JPanel respectivo a la gestion de vehiculos, ademas de una instancia CardLayout para gestionar la navegacion de los JPanel, los JPanel se inicializaron en el JFrame y se añadieron a un JPanel contenedor, ademas de añadio un JMenuBar en el cual se añadieron los JMenuItem respectivo a cada tipo de gestion del sistema, se configuro los JMenuItem de agregar, editar, buscar para acceder a cada JPanel respectivo en la seccion de vehiculos ademas se añadieron metodos de acceso, para el vehiculoshashmap, vehiculo, Buscarvehiculos_JDialog, CardLayout, en el get, para el set VehiculosHashMap, y vehiculo

--- src/GUI/Ejecutables/MainFrame.java
+++ src/GUI/Ejecutables/MainFrame.java
@@ -1,5 +1,9 @@
 
 package GUI.Ejecutables;
+import GUI.Ejecutables.Buscarvehiculos_JDialog;
+import Logico.Vehiculos.*;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
 
 /**
  * 11/08/2025
@@ -9,54 +13,177 @@ public class MainFrame extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(MainFrame.class.getName());
     
-    //Inicializacion de Jpanels
-    MenuPrincipal menuprincipal_jpanel = new MenuPrincipal();
-    GestionEmpleados gestionempleado_jpanel = new GestionEmpleados();
-    GestionClientes gestioncliente_jpanel = new GestionClientes();
-    GestionVehiculos gestionvehiculo_jpanel = new GestionVehiculos();
-    GestionContratos gestioncontrato_jpanel = new GestionContratos();
-    GestionReservas gestionreserva_jpanel = new GestionReservas();
+    //Creacion instancia Jpanels/JDiaogs
+    private Bienvenida_JPanel bienvenida_jpanel;
+    private Agregarvehiculos_JPanel agregarvehiculos_jpanel;
+    private Editarvehiculos_JPanel editarvehiculos_jpanel;
+    private Buscarvehiculos_JDialog buscarvehiculos_jdialog = new Buscarvehiculos_JDialog(this, false);
     
+    //Crear instancias de listas y/o objetos
+    private CardLayout cardlayout;
+    
+    private VehiculosHashMap vehiculosHashMap;
+    private Vehiculo vehiculo;
     
     /**
      * Creates new form MainFrame
      */
-    public MainFrame() {
-        initComponents();        
-    //Inicializar tamaño de Jpanels
-    menuprincipal_jpanel.setSize(1000, 600);
-    gestionempleado_jpanel.setSize(1000, 600);
-    gestioncliente_jpanel.setSize(1000, 600);
-    gestionvehiculo_jpanel.setSize(1000, 600);
-    gestioncontrato_jpanel.setSize(1000, 600);
-    gestionreserva_jpanel.setSize(1000, 600);
+    public MainFrame() {   
+        initComponents();    
     
-    //Inicio por default
-        add(menuprincipal_jpanel);
+    //inicializar listas y/o objetos    
+    cardlayout = new CardLayout();
+    Actual_JPanel.setLayout(cardlayout);
+    
+    vehiculosHashMap = new VehiculosHashMap();   
+    vehiculo=null;    
+    
+    //inicializar instancia Jpanels/JDiaogs    
+    bienvenida_jpanel = new Bienvenida_JPanel();
+    agregarvehiculos_jpanel = new Agregarvehiculos_JPanel(this);    
+    editarvehiculos_jpanel = new Editarvehiculos_JPanel(this);
+    
+    //Agregar los JPanels al JPanel contenedor
+    Actual_JPanel.add(bienvenida_jpanel, "bienvenida");   
+    Actual_JPanel.add(agregarvehiculos_jpanel, "agregarvehiculos");   
+    Actual_JPanel.add(editarvehiculos_jpanel, "editarvehiculos");        
+    
     }
+    
+    public void buscar() {      
+        buscarvehiculos_jdialog.setVehiculosHashMap(vehiculosHashMap);
+        buscarvehiculos_jdialog.setVisible(true);
+    }
+
+    //GETS
+
+    public Buscarvehiculos_JDialog getBuscarvehiculos_jdialog() {
+        return buscarvehiculos_jdialog;
+    }
+   
+    public CardLayout getCardlayout() {
+        return cardlayout;
+    }
+    
+    public JPanel getActual_JPanel() {
+        return Actual_JPanel;
+    }
+      
+    public VehiculosHashMap getVehiculosHashMap() {
+        return vehiculosHashMap;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+    //SETS
+    
+    public void setVehiculosHashMap(VehiculosHashMap vehiculosHashMap) {
+        this.vehiculosHashMap = vehiculosHashMap;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+    
+    
+    
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Actual_JPanel = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
+        jMenu5 = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(1000, 600));
         setMinimumSize(new java.awt.Dimension(1000, 600));
-        setSize(new java.awt.Dimension(1000, 600));
+        setPreferredSize(new java.awt.Dimension(1015, 625));
+        setSize(new java.awt.Dimension(1015, 625));
+        getContentPane().setLayout(new java.awt.CardLayout());
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout Actual_JPanelLayout = new javax.swing.GroupLayout(Actual_JPanel);
+        Actual_JPanel.setLayout(Actual_JPanelLayout);
+        Actual_JPanelLayout.setHorizontalGroup(
+            Actual_JPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 1000, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
+        Actual_JPanelLayout.setVerticalGroup(
+            Actual_JPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 578, Short.MAX_VALUE)
         );
+
+        getContentPane().add(Actual_JPanel, "card2");
+
+        jMenuBar1.setBorder(null);
+        jMenuBar1.setForeground(new java.awt.Color(255, 255, 255));
+        jMenuBar1.setBorderPainted(false);
+
+        jMenu1.setText("Vehiculos");
+
+        jMenuItem1.setText("Agregar");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Editar");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setText("Buscar");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem3);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Clientes");
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Empleados");
+        jMenuBar1.add(jMenu3);
+
+        jMenu4.setText("Reservas");
+        jMenuBar1.add(jMenu4);
+
+        jMenu5.setText("Alquileres");
+        jMenuBar1.add(jMenu5);
+
+        setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+    cardlayout.show(Actual_JPanel, "agregarvehiculos");
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+    cardlayout.show(Actual_JPanel, "editarvehiculos");
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+    buscar();
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     public static void main(String args[]) {
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -78,5 +205,15 @@ public class MainFrame extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(() -> new MainFrame().setVisible(true));
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel Actual_JPanel;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     // End of variables declaration//GEN-END:variables
 }
